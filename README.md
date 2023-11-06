27e80a44db8af381de36679331b3e915f098d100
Firdaussi <D600Active@gmail.com>
2023-11-06 14:29:55 +0000

    All the relevant apps now exist and the secret key has been moved into the environment

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# BRDatabase-Python
-# BRDatabase-Python
+This is the code for the rreplacement www.brdatabase.info
+It is currently in development by a sole developer, so don't expect too much too soon.
+
+
--- website/settings.py
+++ website/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-(=3)ojxp8shc5u)^g=9(n!qb#mixdtvf_1lylw-o!r&rcgh*fz"
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -48,6 +49,8 @@ INSTALLED_APPS = [
     "home",      # Index page and introduction
     "dbase",     # All models - better this way as many apps use many models
     "api",       # API to allow for either use of React and/or data distribution
+
+    "cms",       # Wagtail CMS
 ]
 
 MIDDLEWARE = [
