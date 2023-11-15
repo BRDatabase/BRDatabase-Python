2a404905d4dadce0b2b83bf286e003399027182b
Firdaussi <D600Active@gmail.com>
2023-11-15 10:24:06 +0000

    Added a configured subset of DataTables files

--- home/views.py
+++ home/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from typing import Any
+from django.http import HttpResponse
+from django.views.generic import TemplateView
 
-# Create your views here.
+app_name = 'home'
+
+class HomePage(TemplateView):
+    
+    template_name = "home.html"
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context['data'] = "Context Data"
+
+        return context
