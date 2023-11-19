beb66a1285d51c9354503887b86ad74015e3e929
Firdaussi <D600Active@gmail.com>
2023-11-19 16:49:58 +0000

    Added sentry configuration

--- website/settings.py
+++ website/settings.py
@@ -1,11 +1,18 @@
 from pathlib import Path
 import os
 from decouple import config, Csv
+# settings.py
 import sentry_sdk
 
 sentry_sdk.init(
-    dsn="https://examplePublicKey@o0.ingest.sentry.io/0",
-    enable_tracing=True,
+    dsn="https://64a54e24f1fc2bf2013576d28734285a@o4506253473742848.ingest.sentry.io/4506253475512320",
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    traces_sample_rate=1.0,
+    # Set profiles_sample_rate to 1.0 to profile 100%
+    # of sampled transactions.
+    # We recommend adjusting this value in production.
+    profiles_sample_rate=1.0,
 )
 
 # Decoupled environment settings
